4c036a4bdc34dd6eebc44ec3ee7205970611df5b
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-11 19:39:18 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+require "pry"
+
+def square_array(numbers)
+ squared_array = []
+ numbers.each do |number|
+   squared_array.push(number**2)
+ end
+ squared_array
 end
