66027df0b7e406858a478812d81d2d2a2e1fd6c9
Wendy Harris <wendyv510@hotmail.com>
2019-10-15 20:58:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array=[9,10,16,25]
+  square_array.each do |numbers|
+    puts "#{number} **"
+end
+
+def square_array=[1,2,3]
+  square_array.collect{|x| (x**)}
 end
